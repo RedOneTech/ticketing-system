229e968d4b3214be8a7654c9a316baa88ab73181
unknown <susanto.andrew@ymail.com>
2019-05-16 18:59:42 +0700

    do some php with seating chart table using loop :)

--- seating-chart.php
+++ seating-chart.php
@@ -13,189 +13,83 @@
       }
     </style>
 
-    <title>SMAK 1 Ticketing System</title>
+    <title>Seating Chart</title>
   </head>
   <body>
-    <!-- http://getbootstrap.com/docs/4.1/components/navbar/ -->
-      <nav class="navbar navbar-expand-sm navbar-light bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="#">
-              <img src="images/logo.jpg" width="150" alt="">
-            </a>
-          <button aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" type="button" data-target="#navbarNav" data-toggle="collapse">
-              <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-              <ul class="nav nav-pills">
-                  <li class="nav-item">
-                      <a class="nav-link active" href="index.html" >Home</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link" href="buy">Buy Tickets</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link" href="sheet">Cart</a>
-                  </li>
-              </ul>
-          </div>
-        </div>
-      </nav>
+    <div class="container">
+    <!-- php get POST data -->
+    <?php 
+          $servername = "localhost";
+          $username = "root";
+          $password = "";
+          $dbname = "ticketing-system";
 
-<div class="container">
-  <br>
-  <center>
-<!-- carousel -->
-  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="width:70vw;">
-    <ol class="carousel-indicators">
-      <li data-target="#carouselExampleIndicators" data-slide-to="0" active></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    </ol>
-    <div class="carousel-inner carousel-rounded" style="height:45vh; margin:auto;">
-      <div class="carousel-item active">
-        <img class="d-block w-100 border-radius" src="images/ar-12.jpg" alt="First slide">
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100 border-radius" src="images/b7xQKh.jpg" alt="Second slide">
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100 border-radius" src="images/960x540_lake-sunset-moon-mountains-blue-water-landscape-pictures-desktop.jpg" alt="Third slide">
-      </div>
+          // Create connection
+          $conn = mysqli_connect($servername, $username, $password, $dbname);
+          // Check connection
+          if (!$conn) {
+              die('
+                <div class="alert alert-danger" role="alert">
+                Connection failed: '.mysqli_connect_error().'
+                </div>
+              ');
+          }
+
+          $eventname=$_GET['event'];
+          $sql = 'SELECT column_number, row_number FROM seating_event_info WHERE event_name="'.$eventname.'"';
+          $result = mysqli_query($conn, $sql);
+          $rowresult=mysqli_fetch_assoc($result);
+
+          if(mysqli_num_rows($result)!=0){
+            echo'
+              <br>
+              <div class="row justify-content-center">
+                <h1>'. $eventname.' </h1>
+
+                <!-- for adding break row using white spaces -->
+                <div class="w-100"></div>
+
+                <p class="lead"> Seating Chart </p>
+              </div>
+            ';
+            $column=$rowresult['column_number'];
+            $row=$rowresult['row_number'];
+          }
+          else{
+            echo'
+               <br><br>
+               <div class="alert alert-danger" role="alert">
+               Event Not Found
+               </div>
+            ';
+          }
+
+          mysqli_close($conn); 
+          ?>
+    <!-- event name and slog -->
+    
+    <br>
+
+    <!-- table using php loop -->
+    <center>
+      <table border="1">
+        <?php 
+          if(isset($row)){
+            for($i=1;$i<=$row;$i++){
+              echo'<tr>';
+              for($x=1;$x<=$column;$x++){
+                echo'<td>';
+                echo' <img src="images/seat-available.png" alt="available" height="42" width="42"> ';
+                echo'</td>';
+              }
+              echo'</tr>';
+            } 
+          }
+        ?>
+    
+      </table>
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
-  </center>
-
-  <br><br>
-
-<div class="row justify-content-center">
-  <h1 class="display-4"> Upcoming Event </h1>
-
-  <!-- for adding break row using white spaces -->
-  <div class="w-100"></div>
-
-  <p class="lead"> Select on the event to buy ticket </p>
-</div>
-
-<br>
-<!-- card for multiple event -->
-<div class="row">
-  <div class="col-sm-auto">
-    <div class="card" style="width: 18rem;">
-      <img class="card-img-top" src="images/event-1.jpg" alt="Event-1 mini banner" style="height:30vh">
-      <div class="card-body">
-        <h5 class="card-title">Event 1</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-  <!-- seocnd event -->
-  <div class="col-sm-auto">
-    <div class="card" style="width: 18rem;">
-      <img class="card-img-top" src="images/event-2.jpg" alt="Event-2 mini banner" style="height:30vh">
-      <div class="card-body">
-        <h5 class="card-title">Event 2</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Link to event page </a>
-      </div>
-    </div>
-  </div>
-</div>
-
-<br><br>
-
-<!-- Footer -->
-<footer class="page-footer font-small blue pt-4">
-
-  <!-- Footer Links -->
-  <div class="container-fluid text-center text-md-left">
-
-    <!-- Grid row -->
-    <div class="row">
-
-      <!-- Grid column -->
-      <div class="col-md-6 mt-md-0 mt-3">
-
-        <!-- Content -->
-        <h5 class="text-uppercase">Footer Content</h5>
-        <p>Here you can use rows and columns to organize your footer content.</p>
-
-      </div>
-      <!-- Grid column -->
-
-      <hr class="clearfix w-100 d-md-none pb-3">
-
-      <!-- Grid column -->
-      <div class="col-md-3 mb-md-0 mb-3">
-
-        <!-- Links -->
-        <h5 class="text-uppercase">Links</h5>
-
-        <ul class="list-unstyled">
-          <li>
-            <a href="#!">Link 1</a>
-          </li>
-          <li>
-            <a href="#!">Link 2</a>
-          </li>
-          <li>
-            <a href="#!">Link 3</a>
-          </li>
-          <li>
-            <a href="#!">Link 4</a>
-          </li>
-        </ul>
-
-      </div>
-      <!-- Grid column -->
-
-      <!-- Grid column -->
-      <div class="col-md-3 mb-md-0 mb-3">
-
-        <!-- Links -->
-        <h5 class="text-uppercase">Links</h5>
-
-        <ul class="list-unstyled">
-          <li>
-            <a href="#!">Link 1</a>
-          </li>
-          <li>
-            <a href="#!">Link 2</a>
-          </li>
-          <li>
-            <a href="#!">Link 3</a>
-          </li>
-          <li>
-            <a href="#!">Link 4</a>
-          </li>
-        </ul>
-
-      </div>
-      <!-- Grid column -->
-
-    </div>
-    <!-- Grid row -->
-
-  </div>
-  <!-- Footer Links -->
-
-  <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">© 2019 Copyright:
-    <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
-  </div>
-  <!-- Copyright -->
-
-</footer>
-<!-- Footer -->
-
+    </center>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
